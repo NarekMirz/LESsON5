798aff4d2bc5171946b2fbc0d3de3bf70bba75db
Narek <mirzakhanyan99@list.ru>
2022-12-02 18:39:41 +0300

    Готовое

---
+++ dz34/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+System.Console.WriteLine("Введите количество элементов в массиве ");
+int currentElements;
+currentElements = int.Parse(Console.ReadLine() ?? "");
+int[]myArray = new int [currentElements];
+Random rand = new Random();
+int chetNum = 0;
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rand.Next(100,1000);
+    System.Console.WriteLine(myArray[i] + " ");
+}
+for(int i = 0 ; i < myArray.Length; i++)
+{
+    if(myArray[i] % 2 == 0)
+        chetNum++;
+}
+System.Console.WriteLine("Количество чётных чисел в массиве: " + chetNum);
